cc4984250a985ac2c044ac1c288d096bcaf59607
Uktam1998 <rakhmatullayev2019@mail.ru>
2024-06-26 12:14:14 +0500

    added main Carousel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import CarsCard from '../../components/CarsCard/CarsCard'
+import MainSlider from '../../components/MainSlider/MainSlider'
 function Home() {
   return (
     <>
+      <MainSlider />
       <CarsCard />
     </>
   )
