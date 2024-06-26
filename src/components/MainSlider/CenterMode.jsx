90ef944f6c421ebec4b6707655d43bb5ad03c2b4
Uktam1998 <rakhmatullayev2019@mail.ru>
2024-06-26 17:08:25 +0500

    added main Carousel with text

--- src/components/MainSlider/CenterMode.jsx
+++ src/components/MainSlider/CenterMode.jsx
@@ -1,35 +1,47 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { Component } from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 function CenterMode() {
   const settings = {
-    className: "center",
+    className: 'center',
     centerMode: true,
     infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
-  };
+    centerPadding: '0',
+    slidesToShow: 2,
+    speed: 500,
+  }
   return (
     <div className="slider-container">
       <Slider {...settings}>
         <div>
-          <img src="https://www.autozoomrental.com/static/media/audi.c4adb12ac6dec846adc3.png" alt="Main 1" />
+          <img
+            src="https://www.autozoomrental.com/static/media/audi.c4adb12ac6dec846adc3.png"
+            alt="Main 1"
+          />
         </div>
         <div>
-        <img src="https://www.autozoomrental.com/static/media/mersedez.efa884d1c86e12f4fb0f.png" alt="Main 2" />
+          <img
+            src="https://www.autozoomrental.com/static/media/mersedez.efa884d1c86e12f4fb0f.png"
+            alt="Main 2"
+          />
         </div>
         <div>
-        <img src="https://www.autozoomrental.com/static/media/rolsroys.500642b0161c40ebfcdc.png" alt="Main 3" />
+          <img
+            src="https://www.autozoomrental.com/static/media/rolsroys.500642b0161c40ebfcdc.png"
+            alt="Main 3"
+          />
         </div>
         <div>
-        <img src="https://www.autozoomrental.com/static/media/mersedez.efa884d1c86e12f4fb0f.png" alt="Main 4" />
+          <img
+            src="https://www.autozoomrental.com/static/media/mersedez.efa884d1c86e12f4fb0f.png"
+            alt="Main 4"
+          />
         </div>
       </Slider>
     </div>
-  );
+  )
 }
 
-export default CenterMode;
+export default CenterMode
