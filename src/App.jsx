4cd697f575a2874451b8bffe6b0f00bed24d5760
Uktam1998 <rakhmatullayev2019@mail.ru>
2024-06-27 15:55:48 +0500

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-/* eslint-disable no-unused-vars */
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Cars from './pages/Cars/Cars'
-import Contacts from './pages/Contacts/Contacts'
-import Blog from './pages/Blog/Blog'
-import CarsParam from './pages/CarsParam/CarsParam'
-import Service from './pages/Service/Service'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footerr'
-import { useEffect } from 'react'
-import { use } from 'i18next'
-import Loader from './components/Loader/Loader'
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Cars from './pages/Cars/Cars';
+import Contacts from './pages/Contacts/Contacts';
+import Blog from './pages/Blog/Blog';
+import CarsParam from './pages/CarsParam/CarsParam';
+import Service from './pages/Service/Service';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footerr';
+import Loader from './components/Loader/Loader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    setTimeout(() => {
-      return <Loader />
-    }, 2000)
-  }, [])
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/services" element={<CarsParam/>} />
+        <Route path="/services" element={<CarsParam />} />
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
