6b3f5e34ec77dd72f6be502b10879f135cdb1584
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2024-06-28 11:36:43 +0500

    New Changes applied

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import About from './pages/About/About'
 import Cars from './pages/Cars/Cars'
 import Contacts from './pages/Contacts/Contacts'
 import Blog from './pages/Blog/Blog'
-import CarsParam from './pages/CarsParam/CarsParam'
-import Service from './pages/Service/Service'
+/* import CarsParam from './pages/CarsParam/CarsParam'
+import Service from './pages/Service/Service' */
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footerr'
 //28.06.2024 Changed
