5318ebfdbcada995b04642ed18e88dad18fe3c41
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2024-06-30 15:09:48 +0500

    Changes applied

--- src/components/Brands/BrandSlider.jsx
+++ src/components/Brands/BrandSlider.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import Slider from "react-slick";
-import styles from './BrandSlider.module.css';
+import styles from "./BrandSlider.module.css";
 
 function MultipleRows() {
-  const base_URL = 'https://autoapi.dezinfeksiyatashkent.uz/api';
-  const base_URL2 = 'https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/';
+  const base_URL = "https://autoapi.dezinfeksiyatashkent.uz/api";
+  const base_URL2 =
+    "https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/";
   const [datas, setDatas] = useState([]);
 
   const getFetch = async (url) => {
     const response = await fetch(url, {
-      method: 'GET',
+      method: "GET",
     });
     return await response.json();
   };
@@ -33,23 +34,21 @@ function MultipleRows() {
 
   return (
     <>
-        <h2>Brands</h2>
+      <h2>Brands</h2>
       <div className={styles.slider__container}>
-      {/* <Slider {...settings}>
+        {/* <Slider {...settings}>
       </Slider> */}
-      {datas.map((item) => (
-        <div key={item.id} className={styles.brand__images}>
-          <img
-            src={`${base_URL2}${item.image_src}`}
-            alt={item.title}
-            className={styles.brand__img}
-          />
-          <p className={styles.brand__text}>
-            {item.title}
-          </p>
-        </div>
-      ))}
-    </div>
+        {datas.map((item) => (
+          <div key={item.id} className={styles.brand__images}>
+            <img
+              src={`${base_URL2}${item.image_src}`}
+              alt={item.title}
+              className={styles.brand__img}
+            />
+            <p className={styles.brand__text}>{item.title}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,35 +1,69 @@
-import './Contacts.css';
-import './Contacts_resp.css';
+import "./Contacts.css";
+import "./Contacts_resp.css";
 
 function Contacts() {
   return (
     <div className="Contacts">
-        <div className="contacts_head">
-            <a href="/" className="contacts_head_link"><span>Luxury Cars for Rent in Dubai</span>  /  <span>Contacts</span></a>
-            <h1 className="contacts_header">HAVE ANY QUESTIONS?</h1>
-            <div className='contacts_header_text'>
-                <div>We would love to help</div>
-            <div>Auto Zoom Car Rental Dubai, is the best luxury car rental Dubai based company.</div></div>
+      <div className="contacts_head">
+        <a href="/" className="contacts_head_link">
+          <span>Luxury Cars for Rent in Dubai</span> / <span>Contacts</span>
+        </a>
+        <h1 className="contacts_header">HAVE ANY QUESTIONS?</h1>
+        <div className="contacts_header_text">
+          <div>We would love to help</div>
+          <div>
+            Auto Zoom Car Rental Dubai, is the best luxury car rental Dubai
+            based company.
+          </div>
         </div>
-        <div className="contacts_address">
-            <h4 className='contacts_address_header'>Head Office</h4>
-            <div className='contacts_address_flexed'>
-            <i className="fa-solid fa-location-dot"></i>
-            <span className='contacts_address_info'>Elite 3 Sports City, Dubai 26W8 24J, United Arab Emirates</span>
-            </div>
-            <div className='contacts_address_flexed'>
-            <i className="fa-solid fa-phone"></i>
-            <span className='contacts_address_info'>+971 (55) 846 21 24</span>
-            </div>
-            <div className='contacts_address_flexed'>
-            <i className="fa-solid fa-envelope-open-text"></i>
-            <span className='contacts_address_info'>office@autozoomrental.com</span>
-            </div>
+      </div>
+      <div className="contacts_address">
+        <h4 className="contacts_address_header">Head Office</h4>
+        <div className="contacts_address_flexed">
+          <i className="fa-solid fa-location-dot"></i>
+          <span className="contacts_address_info">
+            Elite 3 Sports City, Dubai 26W8 24J, United Arab Emirates
+          </span>
         </div>
-        <hr className='contacts_hr'/>
-        <div className='forMap'><a href="https://yandex.uz/maps/11499/dubai/?utm_medium=mapframe&utm_source=maps" style={{color:"#eee", fontSize:"12px", position:"absolute", top:"0px"}}></a><a href="https://yandex.uz/maps/11499/dubai/?ll=55.216868%2C25.043460&utm_medium=mapframe&utm_source=maps&z=16.63" style={{color:"#eee", fontSize:"12px", position:"absolute", top:"14px"}}></a><iframe src="https://yandex.uz/map-widget/v1/?ll=55.216868%2C25.043460&z=16.63"  frameBorder="1" allowfullscreen="true" style={{width:"100%", height:"47vh", borderRadius: "23px"}}></iframe>
+        <div className="contacts_address_flexed">
+          <i className="fa-solid fa-phone"></i>
+          <span className="contacts_address_info">+971 (55) 846 21 24</span>
         </div>
+        <div className="contacts_address_flexed">
+          <i className="fa-solid fa-envelope-open-text"></i>
+          <span className="contacts_address_info">
+            office@autozoomrental.com
+          </span>
         </div>
-  )
+      </div>
+      <hr className="contacts_hr" />
+      <div className="forMap">
+        <a
+          href="https://yandex.uz/maps/11499/dubai/?utm_medium=mapframe&utm_source=maps"
+          style={{
+            color: "#eee",
+            fontSize: "12px",
+            position: "absolute",
+            top: "0px",
+          }}
+        ></a>
+        <a
+          href="https://yandex.uz/maps/11499/dubai/?ll=55.216868%2C25.043460&utm_medium=mapframe&utm_source=maps&z=16.63"
+          style={{
+            color: "#eee",
+            fontSize: "12px",
+            position: "absolute",
+            top: "14px",
+          }}
+        ></a>
+        <iframe
+          src="https://yandex.uz/map-widget/v1/?ll=55.216868%2C25.043460&z=16.63"
+          frameBorder="1"
+          allowfullscreen="true"
+          style={{ width: "100%", height: "47vh", borderRadius: "23px" }}
+        ></iframe>
+      </div>
+    </div>
+  );
 }
-export default Contacts
+export default Contacts;
