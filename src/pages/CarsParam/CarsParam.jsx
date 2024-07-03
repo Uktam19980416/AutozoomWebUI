886d758ed5cb7dd5e8bf2fc4619121f16efcc7cd
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2024-07-03 15:14:42 +0500

    Ready

--- src/pages/CarsParam/CarsParam.jsx
+++ src/pages/CarsParam/CarsParam.jsx
@@ -89,7 +89,10 @@ function CarsParam() {
               <p className="greyishText-1">Kilometrage limit per day</p>
               <h4>
                 {data?.max_speed} (Every extra km will be charged
-                <span style={{paddingLeft:"5px"}}>{data?.transmission}</span> AED/km)
+                <span style={{ paddingLeft: "5px" }}>
+                  {data?.transmission}
+                </span>{" "}
+                AED/km)
               </h4>
               <p className="greyishText-1">Car rental deposit amount</p>
               <h4>The deposit is refunded within {data?.deposit} days</h4>
@@ -453,7 +456,7 @@ function CarsParam() {
         </div>
       </div>
       <div className="container__1">
-      <CarsAll getData={getData}/>
+        <CarsAll getData={getData} />
       </div>
     </div>
   );
